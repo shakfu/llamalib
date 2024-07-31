f810d4d041f003c6b43a6faed33db53fc22df397
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2024-07-31 11:58:29 +0300

    snap

--- projects/nbllama/nbllama.cpp
+++ projects/nbllama/nbllama.cpp
@@ -1,7 +1,43 @@
 #include <nanobind/nanobind.h>
 
-int add(int a, int b) { return a + b; }
+#include <llama.h>
 
-NB_MODULE(my_ext, m) {
-    m.def("add", &add);
+#include <memory>
+
+namespace nb = nanobind;
+
+
+
+NB_MODULE(nbllama, m) {
+    m.doc() = "nanobind pyllama wrapper"; // optional module docstring
+
+    nb::class_<llama_token_data>(m, "llama_token_data")
+        // .def( nb::init<>( [](){ return new llama_token_data(); } ))
+        .def(nb::init<>())
+        .def_rw("id", &llama_token_data::id)         // token id
+        .def_rw("logit", &llama_token_data::logit)   // log-odds of the token
+        .def_rw("p", &llama_token_data::p);          // probability of the token
+
+    m.def("llama_context_default_params", (struct llama_context_params (*)()) &llama_context_default_params, "C++: llama_context_default_params() --> struct llama_context_params");
+
+    m.def("llama_model_quantize_default_params", (struct llama_model_quantize_params (*)()) &llama_model_quantize_default_params, "C++: llama_model_quantize_default_params() --> struct llama_model_quantize_params");
+
+    m.def("llama_backend_init", (void (*)()) &llama_backend_init, "C++: llama_backend_init() --> void");
+
+    m.def("llama_numa_init", (void (*)(enum ggml_numa_strategy)) &llama_numa_init, "C++: llama_numa_init(enum ggml_numa_strategy) --> void", nb::arg("numa"));
+
+    m.def("llama_backend_free", (void (*)()) &llama_backend_free, "C++: llama_backend_free() --> void");
+
+    m.def("llama_time_us", (int (*)()) &llama_time_us, "C++: llama_time_us() --> int");
+
+    m.def("llama_max_devices", (int (*)()) &llama_max_devices, "C++: llama_max_devices() --> int");
+
+    m.def("llama_supports_mmap", (bool (*)()) &llama_supports_mmap, "C++: llama_supports_mmap() --> bool");
+
+    m.def("llama_supports_mlock", (bool (*)()) &llama_supports_mlock, "C++: llama_supports_mlock() --> bool");
+
+    m.def("llama_supports_gpu_offload", (bool (*)()) &llama_supports_gpu_offload, "C++: llama_supports_gpu_offload() --> bool");
+
+    m.def("llama_model_quantize", (int (*)(const char *, const char *, const struct llama_model_quantize_params *)) &llama_model_quantize, "C++: llama_model_quantize(const char *, const char *, const struct llama_model_quantize_params *) --> int", nb::arg("fname_inp"), nb::arg("fname_out"), nb::arg("params"));
+
 }
